fe414944732e926f381eb0407fdd0fbd9e574ee2
info <info@info19.cshl.edu>
2019-10-16 11:59:26 -0400

    adding Python_1

---
+++ Python_1/Python_1.3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+Name = sys.argv[1] 
+Color = sys.argv[2]
+Activity = sys.argv[3] 
+Animal = sys.argv[4] 
+
+print("My Name is",Name)    
+print("My favourite color is",Color)    
+print("My favourite activity is",Activity)    
+print("My favourite animal is",Animal)  
