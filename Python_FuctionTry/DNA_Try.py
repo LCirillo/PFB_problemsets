6af26123ab99b09db254fd671b0597a3d223c8ae
info <info@info19.cshl.edu>
2019-10-25 13:35:54 -0400

    adding Pytho_FunctionTry

---
+++ Python_FuctionTry/DNA_Try.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from PyF_CountG	import countG
+
+
+DNA = 'aaagggccggcgaggcgagagcg'
+print(countG(DNA))
+
+
---
+++ Python_FuctionTry/PyF_CountG.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+
+def countG (obj):
+  nG = obj.count('g')
+  return nG
+
